6a1c94139fb5de8e19822b0b4386a240480a4e77
2Raph <r.chapuis5c3@gmail.com>
2025-11-12 15:03:47 +0100

    importation
    bibliothèques importées

--- etape3/etape3.py
+++ etape3/etape3.py
@@ -1,3 +1,10 @@
+import pandas as pd # pour les tableaux
+import matplotlib.pyplot as plt # graphiques
+import tkinter # pour les interfaces graphiques utilisateurs
+
+
+
+
 def etape3_main(result):
     print("Exécution de l'étape 3...")
     # Exemple : display of the result
