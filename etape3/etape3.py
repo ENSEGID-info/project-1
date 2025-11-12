18411942a5898310d3268d7cd6adfc2fc893b6cf
2Raph <r.chapuis5c3@gmail.com>
2025-11-12 15:48:05 +0100

    code graphiques
    on a les codes pour les graphiques, il nous manque à savoir comment accéder aux données

--- etape3/etape3.py
+++ etape3/etape3.py
@@ -4,6 +4,92 @@ import tkinter # pour les interfaces graphiques utilisateurs
 
 
 
+url_request="sideshow.jpl.nasa.gov/pub/JPL_GPS_Timeseries/repro2018a/post/point/7ODM.series"
+
+def graph_Lat(X,Y_lat):
+    plt.plot(X,Y)
+    plt.xlabel("Temps")
+    plt.ylabel("Latitude")
+    plt.title("Latitude en fonction du temps")
+    plt.show()
+
+def graph_Long(X,Y_long):
+    plt.plot(X,Y_long)
+    plt.xlabel("Temps")
+    plt.ylabel("Longitude")
+    plt.title("Longitude en fonction du temps")
+    plt.show()
+
+def graph_Alt(X,Y_alt):
+    plt.plot(X,Y_alt)
+    plt.xlabel("Temps")
+    plt.ylabel("Atitude")
+    plt.title("Altitude en fonction du temps")
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def etape3_main(result):
     print("Exécution de l'étape 3...")
