c8f8dc7c0450d8cd6dc9fe616df21a0420da2af9
Peio <cometpeio@gmail.com>
2025-11-26 17:21:03 +0100

    Raster va carry (hypothétiquement)

--- main.py
+++ main.py
@@ -28,9 +28,48 @@ def main():
 if __name__ == "__main__":
     main()
 
-
+import rasterio
+from rasterio.plot import show
+import numpy as np
+import pandas as pd
 import math as m
 import matplotlib.pyplot as plt
+
+df = pd.DataFrame({
+    "lat": [48.8566, 48.857, 48.855],
+    "lon": [2.3522, 2.353, 2.351],
+    "dE": [0.02, -0.01, 0.03],  
+    "dN": [0.01, 0.02, -0.02]
+})
+raster_path = "C:/Users/pcometducass/Downloads/USGS_DS-424_2.tif"
+
+with rasterio.open(raster_path) as src:
+    img = src.read(1)
+    transform = src.transform
+    crs = src.crs
+# Fonction pour transformer lon/lat → pixels
+def world_to_pixel(x, y, transform):
+    col, row = ~transform * (x, y)
+    return col, row
+
+# Ajouter colonnes px/py dans le DataFrame
+df["px"], df["py"] = zip(*df.apply(lambda r: world_to_pixel(r["lon"], r["lat"], transform), axis=1))
+plt.figure(figsize=(10, 10))
+
+# Affichage du raster
+plt.imshow(img, cmap="gray", origin="upper")
+
+# Affichage vecteurs vitesse
+plt.quiver(
+    df["px"], df["py"],          # positions pixel
+    df["dE"], -df["dN"],         # vecteurs (y inversé car image)
+    angles='xy', scale_units='xy', scale=1, color="red"
+)
+
+plt.title("Carte raster + vecteurs vitesses")
+plt.xlabel("px")
+plt.ylabel("py")
+plt.show()
 
 lat1=-0.209536
 long1=0.640302
@@ -38,10 +77,10 @@ lat2=-0.005106
 long2= 0.003700
 tps1=41045400.00
 tps2=816479850.00
+
 # Programme 1 projet : afficher la flèche et le point pour une station donnée 
 
 #calcul de la vitesse en adaptant la distance parcourut à la surface sphérique de la Terre
-
 
 def vitesse (lat1,lat2,long1,long2,tps1,tps2):
     lat1,lat2,long1,long2=m.radians(lat1),m.radians(lat2),m.radians(long1),m.radians(long2)
@@ -70,6 +109,3 @@ plt.arrow(long1,lat1,arrow_dlong,arrow_dlat,head_width=0.02,head_length=0.03,len
 plt.xlabel("Longitude (°)")
 plt.ylabel("Latitude (°)")
 plt.title("Position GPS sur le plan Latitude = f(Longitude)")
-plt.legend()
-plt.grid(True)
-plt.show()
