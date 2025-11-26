35ffbe14646e908cdc0fbf31c0db0199e4f26d2c
agascou <nanogascou@gmail.com>
2025-11-26 16:48:20 +0100

    mise en forme
    test

---
+++ untitled1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 12 15:43:54 2025
+
+@author: agascou
+"""
+
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+root.title("point en graphe")
+mainframe = ttk.Frame(root, padding=(3, 3, 12, 12))
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
