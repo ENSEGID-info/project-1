4a8eb14d4e1d4980af4e262677c5fca72db0ba50
Zelie_DC <zdaniaucabanillas@gmail.com>
2025-11-27 09:58:28 +0100

    Création fenêtre
    fenêtre vide + texte

---
+++ etape1/programme bien.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from tkinter import *
+
+#ouvre une fenêtre
+fenetre = Tk()
+
+#affiche du texte
+label = Label(fenetre, text="Rentrez le nom du point gps")
+LASTlabel.pack() #permet de le faire apparaître
+
+fenetre.mainloop()
