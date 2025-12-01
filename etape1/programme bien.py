9f084a6ab00f83c22aceeb53821e7964564ae109
ZelieDC <zdaniaucabanillas@gmail.com>
2025-12-01 12:03:51 +0100

    frame + PanedWindow
    = fenêtre pour carte + fenêtre et 3 sous fenêtres pour 3 graphes

--- etape1/programme bien.py
+++ etape1/programme bien.py
@@ -23,5 +23,36 @@ entree.pack()
 bouton=Button(fenetre, text="Tracer", command=print('graph'))
 bouton.pack()
 
+
+#PANEDWINDOW conteneur qui peut contenir autant de panneaux que nécessaire disposé horizontalement ou verticalement.
+p = PanedWindow(fenetre, orient=HORIZONTAL)
+p.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
+p.add(Label(p, text='Volet 1', background='blue', anchor=CENTER))
+p.add(Label(p, text='Volet 2', background='white', anchor=CENTER) )
+p.add(Label(p, text='Volet 3', background='red', anchor=CENTER) )
+p.pack()
+
+fenetre['bg']='white'
+
+# 2 frame = 2 zones conteneurs qui permettent de séparer des éléments, les cartes
+Frame1 = Frame(fenetre, borderwidth=2, relief=GROOVE)
+Frame1.pack(side=LEFT, padx=30, pady=30)
+
+Frame2 = Frame(fenetre, borderwidth=2, relief=GROOVE)
+Frame2.pack(side=LEFT, padx=30, pady=30)
+
+
+#titre frame
+l = LabelFrame(fenetre, text="carte 2D", padx=20, pady=20)
+l.pack(fill="both", expand="yes")
+ 
+Label(l, text="A l'intérieure de la frame 2").pack()
+
+l = LabelFrame(fenetre, text="carte 3D", padx=20, pady=20)
+l.pack(fill="both", expand="yes")
+ 
+Label(l, text="A l'intérieure de la frame").pack()
+
+
 ##fin du pgm, affiche la fenêtre finale
 fenetre.mainloop()
