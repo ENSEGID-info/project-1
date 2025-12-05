12cad08fbc7493cef49ce685a7632d9b7a698e83
ZelieDC <zdaniaucabanillas@gmail.com>
2025-12-05 09:45:05 +0100

    recupère donnée utilisateur
    ajout fonction qui recupère le pts gps rentré dans la barre de recherche !!!

--- etape1/programme bien.py
+++ etape1/programme bien.py
@@ -20,16 +20,18 @@ entree = Entry(fenetre, textvariable="string", width=30)
 entree.pack()
 
 # bouton
-bouton=Button(fenetre, text="Tracer", command=print('graph'))
+bouton=Button(fenetre, text="Tracer", commande=tracer)
 bouton.pack()
 
+def tracer():
+    print("graph")
 
 #PANEDWINDOW conteneur qui peut contenir autant de panneaux que nécessaire disposé horizontalement ou verticalement.
 p = PanedWindow(fenetre, orient=HORIZONTAL)
-p.pack(side=TOP, expand=Y, fill=BOTH, pady=2, padx=2)
-p.add(Label(p, text='Volet 1', background='blue', anchor=CENTER))
-p.add(Label(p, text='Volet 2', background='white', anchor=CENTER) )
-p.add(Label(p, text='Volet 3', background='red', anchor=CENTER) )
+p.pack(side=TOP, expand=Y, fill=BOTH, pady=10, padx=2)
+p.add(Label(p, text='Volet 1', anchor=CENTER) )
+p.add(Label(p, text='Volet 2', anchor=CENTER) )
+p.add(Label(p, text='Volet 3', anchor=CENTER) )
 p.pack()
 
 fenetre['bg']='white'
