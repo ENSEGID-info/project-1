f13a427625a16e22c08068da8085b81a62e8e41c
Melissa22031 <devunmelissa@gmail.com>
2025-12-10 11:17:40 +0100

    déplacement dans le bon programme

--- etape2/etape2.py
+++ etape2/etape2.py
@@ -1,54 +1,31 @@
-import
-import math as m
+###Modèle 3D code 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import pandas as pd
 
-lat1=-0.209536
-long1=0.640302
-lat2=-0.005106
-long2= 0.003700
-tps1=41045400.00
-tps2=816479850.00
-# Programme 1 projet : afficher la flèche et le point pour une station donnée 
+def modele3D():
+    df = pd.read_csv(
+        "M:/projet/programation/7ODM.txt",
+        comment='#',
+        sep="\s+",
+        names=["Decimal_YY","East","North","Vert","col5","col6","col7","col8","col9","Time"]
+    ) #Lecture du fichier
+    
+    x,y,z=df["East"],df["North"],df["Vert"] #récupération des données utiles : longitude, latitude et altitude  
 
-#calcul de la vitesse en adaptant la distance parcourut à la surface sphérique de la Terre
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
+    ax.plot(x, y, z, label='Courbe') 
 
-def vitesse (lat1,lat2,long1,long2,tps1,tps2):
-    lat1,lat2,long1,long2=m.radians(lat1),m.radians(lat2),m.radians(long1),m.radians(long2)
-    R=6371000
-    dlat=R*(lat2-lat1)
-    dlong=R*m.cos(lat1)*(long2-long1)
-    v=(m.sqrt(dlong**2+dlat**2))/(tps2-tps1)
-    return v,dlat,dlong
+    #nomme les axes
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    ax.set_zlabel("Altitude")
+    
+    plt.title("Représentation 3D du déplacement d'une station dans le temps")
 
-# positionner le point de la station et la flèche
-
-
-plt.figure(figsize=(6, 6)) #le graph
-plt.scatter(long1, lat1, color='red', label='Station GPS') #le point
-
-k=0.05 #♣coeff d'échelle à ajuster
-
-v,dlat,dlong=vitesse(lat1,lat2,long1,long2,tps1,tps2)
-
-arrow_dlong = dlong *v*k
-
-arrow_dlat = dlat *v*k
-
-plt.arrow(long1,lat1,arrow_dlong,arrow_dlat,head_width=0.02,head_length=0.03,length_includes_head=True,color='red')
-
-plt.xlabel("Longitude (°)")
-plt.ylabel("Latitude (°)")
-plt.title("Position GPS sur le plan Latitude = f(Longitude)")
-plt.legend()
-plt.grid(True)
-plt.show()
-
-
-
-def etape2_main(data):
-    print("Exécution de l'étape 2...")
-    # Exemple : traitement de la donnée
-    result = max(data)
-    return result
-
+    plt.tight_layout()
+    plt.show() #affiche le graphe 3D 
+    
